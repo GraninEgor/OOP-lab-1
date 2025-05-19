8e36d2b66454e2a168711d917aa601bcf15e96a6
Egor_Granin <epichescktdm@gmail.com>
2025-05-19 16:25:57 +0700

    divided create

--- src/PizzaManager.java
+++ src/PizzaManager.java
@@ -15,48 +15,71 @@ public class PizzaManager extends Manager<Pizza> implements ManagerFunctionality
     }
 
     @Override
-    public void create(){
-        ArrayList<Ingredient> pizzaIngredients = new ArrayList<>();
-        Base pizzaBase;
-        Ingredient pizzaIngredient;
-        boolean baseDialog = true;
-        boolean ingredientDialog = true;
-        int selectedBase;
-        int selectedIngredient;
-        String pizzaName;
+    public void create() {
+        String pizzaName = getNewPizzaName();
+        if (pizzaName == null) return;
+
+        Base selectedBase = selectBaseFromUser();
+        if (selectedBase == null) return;
+
+        ArrayList<Ingredient> ingredients = selectIngredientsFromUser();
+        if (ingredients == null) return;
+
+        storage.add(new Pizza(pizzaName, 0, selectedBase, ingredients));
+        System.out.println("Пицца успешно создана!");
+    }
+
+    private String getNewPizzaName() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Введите название пиццы");
-        pizzaName = scanner.nextLine();
-        while (baseDialog){
-            System.out.println("Выберите основу\nЧтобы вернутся нажмите -1");
+        return scanner.nextLine();
+    }
+
+    private Base selectBaseFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        Base selectedBase = null;
+        boolean baseDialog = true;
+
+        while (baseDialog) {
+            System.out.println("Выберите основу\nЧтобы вернуться нажмите -1");
             baseManager.print();
-            selectedBase = scanner.nextInt();
-            if(selectedBase>=0 && selectedBase<baseManager.storageSize()){
-                pizzaBase = baseManager.storageGet(selectedBase);
-                while (ingredientDialog){
-                    System.out.println("Выбери ингредиенты:\nДля подтвердения введите -1");
-                    ingredientManager.print();
-                    selectedIngredient = scanner.nextInt();
-                    if(selectedIngredient>=0 && selectedIngredient<ingredientManager.storageSize()){
-                        pizzaIngredients.add(ingredientManager.storageGet(selectedIngredient));
-                    }
-                    else if(selectedIngredient == -1){
-                        storage.add(new Pizza(pizzaName,0,pizzaBase,pizzaIngredients));
-                        ingredientDialog = false;
-                        baseDialog = false;
-                    }
-                    else{
-                        System.out.println("Некорректный ввод");
-                    }
-                }
-            }
-            else if(selectedBase == -1){
+
+            int selectedBaseIndex = scanner.nextInt();
+            if (selectedBaseIndex >= 0 && selectedBaseIndex < baseManager.storageSize()) {
+                selectedBase = baseManager.storageGet(selectedBaseIndex);
                 baseDialog = false;
-            }
-            else{
+            } else if (selectedBaseIndex == -1) {
+                return null;
+            } else {
                 System.out.println("Некорректный ввод");
             }
         }
+
+        return selectedBase;
     }
+
+    private ArrayList<Ingredient> selectIngredientsFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Ingredient> pizzaIngredients = new ArrayList<>();
+        boolean ingredientDialog = true;
+
+        while (ingredientDialog) {
+            System.out.println("Выберите ингредиенты:\nДля подтверждения введите -1");
+            ingredientManager.print();
+
+            int selectedIngredientIndex = scanner.nextInt();
+            if (selectedIngredientIndex >= 0 && selectedIngredientIndex < ingredientManager.storageSize()) {
+                pizzaIngredients.add(ingredientManager.storageGet(selectedIngredientIndex));
+            } else if (selectedIngredientIndex == -1) {
+                ingredientDialog = false;
+            } else {
+                System.out.println("Некорректный ввод");
+            }
+        }
+
+        return pizzaIngredients;
+    }
+
     @Override
     public void update(){
 
